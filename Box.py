037537f65b931a8a39c3ff28e509721310c422b9
Nikita Melnyk <melnyk.nikita.invisible@gmail.com>
2023-12-12 12:03:29 +0100

    [UnitTest[ BoxTest
    Added unit test for box

--- Box.py
+++ Box.py
@@ -1,6 +1,14 @@
 class Box:
 
     def __init__(self, width, length, height):
-        self.width = width
-        self.length = length
-        self.height = height
+        self.__width = width
+        self.__length = length
+        self.__height = height
+
+    def get_width(self): return self.__width
+    def get_height(self): return self.__height
+    def get_length(self): return self.__length
+    def set_width(self, width): self.__width = width
+    def set_length(self, length): self.__length = length
+    def set_height(self, height): self.__height = height
+    def volume(self): return self.__width * self.__length * self.__height
