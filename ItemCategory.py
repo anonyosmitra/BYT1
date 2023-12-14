4d377973bfb1fdc8e139695363d43e8084fa78fb
Anonyo <32406693+anonyosmitra@users.noreply.github.com>
2023-12-14 12:30:16 +0100

    Update ItemCategory.py

--- ItemCategory.py
+++ ItemCategory.py
@@ -1,20 +1,17 @@
 class ItemCategory:
 
     def __init__(self, name: str) -> None:
-        self.__name = name
+        self.set_name(name)
 
     def get_name(self) -> str:
         return self.__name
 
     def set_name(self, name: str) -> bool:
         if name is None or name == "":
-            return False
+            raise Exception("Invalid Name")
 
         self.__name = name
         return True
 
     def __str__(self) -> str:
         return self.__name
-
-    def is_valid(self) -> bool:
-        return self.__name is not None and self.__name != ""
